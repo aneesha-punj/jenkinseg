ca57130d53a8b9d0f16086548dd7240e30905628
unknown <aneesha.punj@gmail.com>
2020-07-28 12:58:18 -0700

    first jenkins example

---
+++ PracticeCodility/KthLargest.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeCodility
+{
+    class KthLargest
+    {
+        public int FindKthLargest(int[] nums, int k)
+        {
+            Array.Sort(nums);
+            int count = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i + 1] > nums[i])
+                {
+                    count++;
+                }
+                if (count == (k - 1))
+                {
+                    return nums[i];
+                }
+
+            }
+            return nums[nums.Length];
+        }
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
+public class Solution
+{
+    public int FindKthLargest(int[] nums, int k)
+    {
+        Array.Sort(nums);
+
+        if (nums[i] < nums[i + 1])
+        {
+            return nums[nums.Length - k];
+        }
+        else
+        {
+            int[] dist = nums.Distinct().ToArray();
+            return dist[dist.Length - k];
+        }
+
+
+    }
+}
---
+++ PracticeCodility/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeCodility
+{
+    class Program
+    {
+        int MaxEven(int[] A)
+        {
+            int sum = 0;
+            int maxEvenSum = 0;
+            for (int i = 0; i < A.Length; i++)
+            {
+                if(A[i]>0)
+                {
+                    sum = sum + A[i];
+                }
+                
+            }
+            if (sum % 2 == 0)
+            {
+                maxEvenSum = sum;
+            }
+            for (int i = 0; i < A.Length; ++i)
+            {
+                if(A[i]%2!=0)
+                {
+                    if(A[i]>0)
+                    {
+                        maxEvenSum = (maxEvenSum > (sum - A[i])) ? maxEvenSum : (sum - A[i]);
+                    }
+                    else
+                    {
+                        maxEvenSum = (maxEvenSum) > (sum + A[i]) ? maxEvenSum : (sum + A[i]);
+                    }
+                }
+                Console.WriteLine(maxEvenSum);
+                Console.ReadLine();
+            }
+            return maxEvenSum;
+        }
+        
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            int[] A = { -2, 2, -3, 1, 3 };
+            p.MaxEven(A);
+           
+        }
+    }
+}
